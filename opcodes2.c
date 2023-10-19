bc372d0d6e08c7124d1b598c4dfb883b74ce972e
hailegnaw27 <ixvos406@gmail.com>
2023-10-19 09:16:05 -0700

    final

--- opcodes2.c
+++ opcodes2.c
@@ -24,7 +24,7 @@ void add(stack_t **stack, unsigned int line_number)
 {
     if (*stack == NULL || (*stack)->next == NULL)
     {
-        fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
+        fprintf(stderr, "Error: L%d: can't add, stack too short\n", line_number);
         exit(EXIT_FAILURE);
     }
 
